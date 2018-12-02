de4683bb7affc28df9711337e1b7e872d5c3f1c3
qdcxdz <42975362+qdcxdz@users.noreply.github.com>
2018-12-02 12:54:47 -0500

    process login

---
+++ process-login.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+//receive username and passowrd
+$email = $_POST['email'];
+$pw = $_POST['pw'];
+
+
+$dsn = "mysql:host=localhost;dbname=de-creation;charset=utf8mb4";
+$dbusername = "xxx"; 
+$dbpassword = "xxxxxxxx";
+
+$pdo = new PDO($dsn, $dbusername, $dbpassword); 
+
+$stmt = $pdo->prepare("SELECT * FROM `login` WHERE `email` = '$email' AND `password` = '$password'");
+
+$stmt->execute();
+
+if($row = $stmt->fetch()){
+	//start session if valid and redirect to dashboard
+	$_SESSION['logged-in'] = true;
+	$_SESSION['email'] = $row['email'];
+	$_SESSION['pw'] = $row['password'];
+	$_SESSION['id'] = $row['id'];
+
+	header("Location: homepage.php");
+
+}else{
+	//redirect to login page if invalid
+	header("Location: homepage.html");
+}
+?>
